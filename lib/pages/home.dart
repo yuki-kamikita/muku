1cac780c66421ac202d2808122cbe5d712b23c7e
yuki_kamikita <akaiyukiusagi@gmail.com>
2022-03-26 20:20:39 +0900

    Riverpod導入

---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class Home extends HookConsumerWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final counter = useState(0);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter example')),
+      body: Center(
+        child: Text('${counter.value}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // readメソッドは、プロバイダーをリッスンせずに読み取るためのユーティリティです。
+        onPressed: () => counter.value++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
