acb963b0c6c2a25020234b67e5d553e738342cd0
yuki_kamikita <akaiyukiusagi@gmail.com>
2022-04-14 23:52:27 +0900

    戦闘のガワだけ追加

--- lib/ui/pages/actions/destination.dart
+++ lib/ui/pages/actions/destination.dart
@@ -7,13 +7,27 @@ class Destination extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
-      appBar: AppBar(title: const Text("行先")),
-      body: Center(
-        child: Text("どこにいく？"),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: MediaQuery.of(context).size.height,
+            // toolbarHeight: 80,
+            pinned: true,
+            // elevation: 2,
+            automaticallyImplyLeading: false, // 戻るボタン非表示 これどうしようかな
+            flexibleSpace:
+                Image.asset('assets/images/dream.jpg', fit: BoxFit.cover),
+          ),
+          SliverList(
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                  padding: EdgeInsets.all(16.0), child: Text('Row_$index'));
+            }),
+          )
+        ],
       ),
     );
   }
-
 }
--- lib/ui/pages/battle_archive.dart
+++ lib/ui/pages/battle_archive.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// 戦闘結果
@@ -7,13 +9,68 @@ class BattleArchive extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
-      appBar: AppBar(title: const Text("戦闘結果")),
-      body: Center(
-        child: Text("戦闘結果"),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: MediaQuery.of(context).size.height,
+            pinned: true,
+            flexibleSpace:
+                Image.asset('assets/images/dream.jpg', fit: BoxFit.cover),
+          ),
+          _turnWidget(context, 1),
+          _turnWidget(context, 2),
+          _turnWidget(context, 3),
+          _turnWidget(context, 4),
+          _turnWidget(context, 5),
+          SliverStickyHeader(
+            header: Container(
+                height: 60.0,
+                color: Theme.of(context).colorScheme.background,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                alignment: Alignment.centerLeft,
+                child: Center(
+                  child: Text(
+                    'Fin',
+                    style: GoogleFonts.parisienne(
+                      textStyle: Theme.of(context).textTheme.titleLarge,
+                      fontSize: 32.0,
+                    ),
+                  ),
+                )),
+          ),
+        ],
       ),
     );
   }
 
+  SliverStickyHeader _turnWidget(BuildContext context, int turn) {
+    return SliverStickyHeader(
+      header: Container(
+          height: 60.0,
+          color: Theme.of(context).colorScheme.background,
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          alignment: Alignment.centerLeft,
+          child: Center(
+            child: Text(
+              'Turn $turn',
+              style: GoogleFonts.parisienne(
+                textStyle: Theme.of(context).textTheme.titleLarge,
+                fontSize: 32.0,
+              ),
+            ),
+          )),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, i) => ListTile(
+            leading: CircleAvatar(
+              child: Text('0'),
+            ),
+            title: Text('行動 #$i'),
+          ),
+          childCount: 15,
+        ),
+      ),
+    );
+  }
 }
